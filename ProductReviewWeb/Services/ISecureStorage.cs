59734d68a18f5ab34d615a92e64799592960974b
Alfredoglez1823 <45645522+Alfredoglez1823@users.noreply.github.com>
2024-07-03 13:30:26 -0600

    w

--- ProductReviewWeb/Services/ISecureStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductReviewWeb.Services
-{
-    public interface ISecureStorage
-    {
-        Task SetItemAsync(string key, string value);
-        Task<string> GetItemAsync(string key);
-        Task RemoveItemAsync(string key);
-    }
-}
